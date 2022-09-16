9e94e8f46fd8489782fc47ebd7d79e6d4328aaf0
MackanRules <marcusg3@kth.se>
2022-09-16 12:23:24 +0200

    Steg 7 i labben

--- ConsoleApp for labb1/ConsoleApp for labb1/Program.cs
+++ ConsoleApp for labb1/ConsoleApp for labb1/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Steg 7 i labben, gör en ändring");
         }
     }
 }
